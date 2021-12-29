6a50af5f5f7a43656fa80e36fb306b9612f34479
Nate Sheridan <83609521+natesheridan@users.noreply.github.com>
2021-12-28 19:38:52 -0700

    Working iFrame

--- src/Pages/Projects/Projects.js
+++ src/Pages/Projects/Projects.js
@@ -11,15 +11,17 @@ const Projects = () => {
 
     return (
     <>
-        <h2>projects directory</h2>
+        <h2>projects</h2>
         <section className="content projects">
             <IframeResizer 
                 id='projectsFrame' 
                 heightCalculationMethod="lowestElement"
-                inPageLinks
-                log
-                src="http://projects.n8s.pw/"
+                autoResize = 'true'
+                inPageLinks='true'
+                checkOrigin="false"
+                src="https://projects.n8s.pw/"
                 style={{ width: '1px', minWidth: '100%'}}
+                // warningTimeout='2000'
             />
         </section>
     </>
