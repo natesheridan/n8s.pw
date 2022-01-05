388b5032a46f54ab3ce4ebf9f71693510000ed28
Nate Sheridan <83609521+natesheridan@users.noreply.github.com>
2022-01-05 02:02:18 -0700

    ansi art

--- src/App.js
+++ src/App.js
@@ -11,13 +11,37 @@ import ScrollArrow from './Components/ScrollArrow/ScrollArrow.js'
 
 function App() {
 
-
+  console.log(`NATE SHERIDAN 2022
+  n8s.pw // n8.wtf // autofoc.us                                                     
+                                                      
+                       888888888                      
+                     88:::::::::88                    
+                   88:::::::::::::88                  
+                  8::::::88888::::::8                 
+nnnn  nnnnnnnn    8:::::8     8:::::8    ssssssssss   
+n:::nn::::::::nn  8:::::8     8:::::8  ss::::::::::s  
+n::::::::::::::nn  8:::::88888:::::8 ss:::::::::::::s 
+nn:::::::::::::::n  8:::::::::::::8  s::::::ssss:::::s
+n:::::nnnn:::::n 8:::::88888:::::8  s:::::s  ssssss 
+n::::n    n::::n8:::::8     8:::::8   s::::::s      
+n::::n    n::::n8:::::8     8:::::8      s::::::s   
+n::::n    n::::n8:::::8     8:::::8ssssss   s:::::s 
+n::::n    n::::n8::::::88888::::::8s:::::ssss::::::s
+n::::n    n::::n 88:::::::::::::88 s::::::::::::::s 
+n::::n    n::::n   88:::::::::88    s:::::::::::ss  
+nnnnnn    nnnnnn     888888888       sssssssssss    
+                                 
+ Thanks for visiting my website! Please let me know if you find any bugs!
+ Contact me through the contact page!                                
+          
+                                 
+                                 `)
 
   
   return(
     <>
     <ScrollArrow />
-    <main onScroll={console.log("Scroll")} >
+    <main>
         <Route exact path = {['/projects', '/about', '/contact']} render = {() => <Header />} />
       <div className="content-container">
         <Route exact path = {['/home', '/']} render = {() => <Home />} />
